d02d7dfd1b56b2171bbe4dba500efd5942271ce9
NelsonRz-afk <nelsruiz9@gmail.com>
2024-12-02 21:53:36 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 $servidor = 'localhost';
 $usuario = 'root';
-$contrase~na = '';
+$password = '';
 $base_datos = 'registros';
-$conexion = new mysqli($servidor, $usuario, $contrase~na,
+$conexion = new mysqli($servidor, $usuario, $password,
 $base_datos);
 if ($conexion->connect_error) {
 die('Error de conexi´on: ' . $conexion->connect_error);
